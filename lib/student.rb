c6ac3f826c44cb822bd060dabf29b605b553383c
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-02-17 08:37:24 +0300

    Done

---
+++ lib/student.rb
@@ -0,0 +1,17 @@
+# will be a super class
+class Student
+
+    def hello
+        # puts out the phrase 
+        # ey there! I'm so excited to learn stuff."
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand
+        # puts out ey there! I'm so excited to learn stuff."
+        puts "Pick me!"
+
+    end
+
+    
+end
